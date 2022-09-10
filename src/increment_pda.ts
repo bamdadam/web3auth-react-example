8c6e44cc5f92a2db59b5a3571505a25fd1ce182c
bamdadam <kanra252@gmail.com>
2022-09-10 13:04:40 +0430

    init

---
+++ src/increment_pda.ts
@@ -0,0 +1,157 @@
+export type IncrementPda = {
+  "version": "0.1.0",
+  "name": "increment_pda",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [
+        {
+          "name": "payer",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "counter",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "authority",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "bump",
+          "type": "u8"
+        }
+      ]
+    },
+    {
+      "name": "increment",
+      "accounts": [
+        {
+          "name": "counter",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "authority",
+          "isMut": false,
+          "isSigner": true
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "counter",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "count",
+            "type": "u64"
+          },
+          {
+            "name": "authority",
+            "type": "publicKey"
+          }
+        ]
+      }
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "SeedsTooLong",
+      "msg": "seeds creating the pda are too long"
+    }
+  ]
+};
+
+export const IDL: IncrementPda = {
+  "version": "0.1.0",
+  "name": "increment_pda",
+  "instructions": [
+    {
+      "name": "initialize",
+      "accounts": [
+        {
+          "name": "payer",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "counter",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "authority",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "bump",
+          "type": "u8"
+        }
+      ]
+    },
+    {
+      "name": "increment",
+      "accounts": [
+        {
+          "name": "counter",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "authority",
+          "isMut": false,
+          "isSigner": true
+        }
+      ],
+      "args": []
+    }
+  ],
+  "accounts": [
+    {
+      "name": "counter",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "count",
+            "type": "u64"
+          },
+          {
+            "name": "authority",
+            "type": "publicKey"
+          }
+        ]
+      }
+    }
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "SeedsTooLong",
+      "msg": "seeds creating the pda are too long"
+    }
+  ]
+};
---
+++ src/solanaRPC.ts
@@ -0,0 +1,213 @@
+import * as anchor from "@project-serum/anchor";
+import { Connection, Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { CustomChainConfig, SafeEventEmitterProvider } from "@web3auth/base";
+import { SolanaWallet } from "@web3auth/solana-provider";
+import { Program } from "@project-serum/anchor";
+import idl from "./idl.json"
+import {IDL} from "./increment_pda";
+import { bs58 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+class swallet extends SolanaWallet {
+  publicKey:PublicKey;
+  constructor(publicKey:PublicKey, provider: SafeEventEmitterProvider) {
+    super(provider);
+    this.publicKey = publicKey;
+}
+}
+let counter1Pda: PublicKey;
+let counter1Bump: number;
+export default class SolanaRpc {
+  private provider: SafeEventEmitterProvider;
+  private programID = new PublicKey(idl.metadata.address);
+  private wallet:any;
+  private isWalletInit = false;
+  private program:any;
+  constructor(provider: SafeEventEmitterProvider) {
+    this.provider = provider;
+  }
+
+  incrementCounter = async (): Promise<string[]> => {
+    if (!this.isWalletInit) {
+      console.log("making anchor program");
+      
+      await this.initWallet();
+    }
+    try {
+      const solanaWallet = new SolanaWallet(this.provider);
+      const acc = await solanaWallet.requestAccounts();
+      [counter1Pda, counter1Bump] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          this.program.provider.wallet.publicKey.toBuffer()
+        ],
+        this.program.programId,
+      );      
+      let sig = await this.program.methods.increment()
+                                          .accounts({
+                                            counter:counter1Pda,
+                                            authority:this.program.provider.wallet.publicKey
+                                          })
+                                          .signers([])
+                                          .rpc();
+      return sig;
+    } catch (error) {
+      return error as string[];
+    }
+  };
+
+  initWallet = async (): Promise<string> => {
+    try {
+      const solanaWallet = new SolanaWallet(this.provider);
+      const userAcc = await solanaWallet.requestAccounts();
+      const connectionConfig = await solanaWallet.request<CustomChainConfig>({ method: "solana_provider_config", params: [] });
+      const conn = new Connection(connectionConfig.rpcTarget);
+      const seed = new PublicKey(userAcc[0]);
+      const wallet = new swallet(seed, this.provider);
+      const provider = new anchor.AnchorProvider(conn, wallet, anchor.AnchorProvider.defaultOptions());
+      anchor.setProvider(provider);
+      this.program = new Program(IDL, this.programID,provider);
+      console.log("init wallet done");
+      return "done";
+    } catch (error) {
+      return error as string;
+    }
+  };
+
+  initCounter = async (): Promise<string> => {
+    if (!this.isWalletInit) {
+      console.log("making anchor program");
+      
+      await this.initWallet();
+    }
+    const solanaWallet = new SolanaWallet(this.provider);
+    const userAcc = await solanaWallet.requestAccounts();
+    const seed = new PublicKey(userAcc[0]);
+    try {
+      [counter1Pda, counter1Bump] = await anchor.web3.PublicKey.findProgramAddress(
+        [
+          seed.toBuffer()
+        ],
+        this.program.programId,
+      );      
+      let tri = await this.program.methods.initialize(
+        counter1Bump,
+      ).accounts({
+        payer:new PublicKey(userAcc[0]),
+        counter: counter1Pda,
+        systemProgram:anchor.web3.SystemProgram.programId,
+        authority: new PublicKey(userAcc[0])
+      }).rpc();
+      return tri;
+    //   const latestblockhash = await conn.getLatestBlockhash()
+    // const trx = new Transaction({
+    //   feePayer:wallet.publicKey,
+    //   blockhash:latestblockhash.blockhash,
+    //   lastValidBlockHeight:latestblockhash.lastValidBlockHeight
+    // }).add(tri);
+    // const { blockhash } = await conn.getRecentBlockhash("finalized");
+    // const transaction = new Transaction({ recentBlockhash: blockhash, feePayer: new PublicKey(seed) }).add(tri).sign(wallet);
+    // const { signature } = await solanaWallet.signAndSendTransaction(transaction);
+    // const {signature} = await solanaWallet.signAndSendTransaction(trx);
+    // const  signature  = await this.provider.request({
+    //     method: "signAndSendTransaction",
+    //     params: {
+    //     message: bs58.encode(trx.serializeMessage()),
+    //   },
+    // });
+    // let sig = await conn.getSignatureStatus(signature as string);
+    // console.log(sig);
+    // const signedTx = await solanaWallet.signTransaction(trx);
+    // return signedTx.signature?.toString() || "";
+    // await program.rpc
+    // return signature;
+    
+    } catch (error) {
+      return error as string;
+    }
+  };
+
+
+
+  getAccounts = async (): Promise<string[]> => {
+    try {
+      const solanaWallet = new SolanaWallet(this.provider);
+      const acc = await solanaWallet.requestAccounts();
+      return acc;
+    } catch (error) {
+      return error as string[];
+    }
+  };
+
+  getBalance = async (): Promise<string> => {
+    try {
+      const solanaWallet = new SolanaWallet(this.provider);
+      const connectionConfig = await solanaWallet.request<CustomChainConfig>({ method: "solana_provider_config", params: [] });
+      const conn = new Connection(connectionConfig.rpcTarget);
+
+      const accounts = await solanaWallet.requestAccounts();
+      const balance = await conn.getBalance(new PublicKey(accounts[0]));
+      return balance.toString();
+    } catch (error) {
+      return error as string;
+    }
+  };
+
+  signMessage = async (): Promise<string> => {
+    try {
+      const solanaWallet = new SolanaWallet(this.provider);
+      const msg = Buffer.from("Test Signing Message ", "utf8");
+      const res = await solanaWallet.signMessage(msg);
+      return res.toString();
+    } catch (error) {
+      return error as string;
+    }
+  };
+
+  sendTransaction = async (): Promise<string> => {
+    try {
+      const solanaWallet = new SolanaWallet(this.provider);
+      const connectionConfig = await solanaWallet.request<CustomChainConfig>({ method: "solana_provider_config", params: [] });
+      const conn = new Connection(connectionConfig.rpcTarget);
+
+      const pubKey = await solanaWallet.requestAccounts();
+      const { blockhash } = await conn.getRecentBlockhash("finalized");
+      const TransactionInstruction = SystemProgram.transfer({
+        fromPubkey: new PublicKey(pubKey[0]),
+        toPubkey: new PublicKey(pubKey[0]),
+        lamports: 0.01 * LAMPORTS_PER_SOL,
+      });
+      const transaction = new Transaction({ recentBlockhash: blockhash, feePayer: new PublicKey(pubKey[0]) }).add(TransactionInstruction);
+      const { signature } = await solanaWallet.signAndSendTransaction(transaction);
+      return signature;
+    } catch (error) {
+      return error as string;
+    }
+  };
+
+  signTransaction = async (): Promise<string> => {
+    try {
+      const solanaWallet = new SolanaWallet(this.provider);
+      const connectionConfig = await solanaWallet.request<CustomChainConfig>({ method: "solana_provider_config", params: [] });
+      const conn = new Connection(connectionConfig.rpcTarget);
+
+      const pubKey = await solanaWallet.requestAccounts();
+      const { blockhash } = await conn.getRecentBlockhash("finalized");
+      const TransactionInstruction = SystemProgram.transfer({
+        fromPubkey: new PublicKey(pubKey[0]),
+        toPubkey: new PublicKey(pubKey[0]),
+        lamports: 0.01 * LAMPORTS_PER_SOL,
+      });
+      const transaction = new Transaction({ recentBlockhash: blockhash, feePayer: new PublicKey(pubKey[0]) }).add(TransactionInstruction);
+      const signedTx = await solanaWallet.signTransaction(transaction);
+      return signedTx.signature?.toString() || "";
+    } catch (error) {
+      return error as string;
+    }
+  };
+
+  getPrivateKey = async (): Promise<string> => {
+    const privateKey = await this.provider.request({
+      method: "solanaPrivateKey",
+    });
+
+    return privateKey as string;
+  };
+}
